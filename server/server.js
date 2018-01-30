888fe1d24e235ea268353b33d093998b1fd0f366
Ala'a Mezian <alaa.mezian.mail@gmailcom>
2018-01-30 13:14:59 +0200

    emiting stuff

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO =require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 var app = express();
+var server =http.createServer(app);
+var io =socketIO(server); //we are telling the  socket to uwse our server
+
+
 const port=process.env.PORT || 8080;
 app.use(express.static(publicPath));
+ 
+io.on('connection',(socket) => 
+{
+    console.log('new user connected');
 
-app.listen(port,()=>
+socket.on('disconnect',()=>{
+    console.log('user was disconnected');
+})
+
+})
+server.listen(port,()=>
 {
     console.log("server is up on port " + port);
 }) 
